4ccbe403a542027d9f8e87169d490324cf95a44d
ClaireShi28 <57731564+ClaireShi28@users.noreply.github.com>
2020-02-09 21:48:59 -0500

    Add files via upload

---
+++ Assignment1/Code/LoadData.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def load_wine_quality_data():
+
+    data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv',
+                       sep=';')
+    print("Wine Quality Data Description")
+    print(data.describe())
+    data = data.dropna()
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+    y_original=data.iloc[:,-1]
+    y = y.values
+    y = y.astype(int)
+    y[y < 6] = 0
+    y[y >= 6] = 1
+
+    return X, y
+
+def load_ozone_data():
+    data = pd.read_csv('https://raw.githubusercontent.com/ClaireShi28/MachineLearning/master/Assignment1/Data/PM2.5.csv',
+                       error_bad_lines=False, sep=",")
+    print("Air Pollution Data Description")
+    print(data.describe())
+    data = data.dropna()
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+    y_original = data.iloc[:, -1]
+    y = y.values
+    #y = y.astype(float)
+    y[y <= 22] = 0 #22 for PM2.5
+    y[y > 22] = 1
+
+    return X, y
+
+
+
+
---
+++ Assignment1/Code/main.py
@@ -0,0 +1,44 @@
+import LoadData
+import KNN
+import DecisionTree as DT
+import NeuralNetwork as NN
+import AdBoost as AB
+import SVM
+import Experiments
+import time
+
+def Process_wine_quality():
+
+    t0=time.time()
+    X, y = LoadData.load_wine_quality_data()
+    op=True
+
+    Experiments.Models_Comparison(X,y,"Wine Quality")
+
+    DT.DecisionTree(X, y, title="Wine Quality Decision Tree",optimize=op)
+    AB.AdaBoost(X, y, title="Wine Quality AdaBoost",optimize=op)
+    KNN.KNN(X, y, title="Wine Quality KNN",optimize=op)
+    NN.NeuralNetwork(X, y, title="Wine Quality Neural Network",optimize=op)
+    SVM.SVM(X, y, title="Wine Quality SVM",optimize=op)
+
+    t1=time.time()
+    print("total TIME")
+    print(t1-t0)
+
+
+def Process_air_quality():
+    X, y = LoadData.load_ozone_data()
+
+    Experiments.Models_Comparison(X, y, "Air Pollution")
+    op=True
+
+    DT.DecisionTree(X, y, title="Air Pollution Decision Tree",optimize=op)
+    AB.AdaBoost(X, y, title="Air Pollution AdaBoost",optimize=op)
+    KNN.KNN(X, y, title="Air Pollution KNN",optimize=op)
+    NN.NeuralNetwork(X, y, title="Air Pollution Neural Network",optimize=op)
+    SVM.SVM(X, y, title="Air Pollution SVM",optimize=op)
+
+if __name__ == "__main__":
+    Process_air_quality()
+    Process_wine_quality()
+
